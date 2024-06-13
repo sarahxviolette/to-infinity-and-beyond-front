8c3ade5db301feaed10c4f1ae61bf326412e19ff
Sarah Violette Akamiezan <sarah.akamiezan@gmail.com>
2024-06-14 01:13:50 +0200

    murphy and millenlium falcon are plugged and display cute outputs ✨

--- pages/Models.py
+++ pages/Models.py
@@ -57,13 +57,16 @@ def calc_distance_earth_obj(V):
     return round(d * Mpc)
 
 def display_prediction(classif, v):
-    print(classif,v,predict_class(classif),calc_velocity(v),calc_distance_earth_obj(v))
     return st.write(f'This is a **{classif}**! Traveling at **{calc_velocity(v):,}** km/s and being **{calc_distance_earth_obj(v):,}** million-years away from us.')
 
 
 def get_random_data_for_tabular():
     datum = pd.read_csv('./data/MF.csv')
     return datum.sample()
+
+def get_random_data_for_hybrid():
+    datum = pd.read_csv('./data/MF_sample.csv')
+    return datum
 
 set_background('./static/galaxy.png')
 
@@ -78,7 +81,7 @@ code = """
     }
 
     h3 {
-        font-size: 3.25em;
+        font-size: 3em;
     }
 
     p {
@@ -151,6 +154,7 @@ with images:
 
 
 filling_data = get_random_data_for_tabular()
+filling_data_hybrid = get_random_data_for_hybrid()
 
 with tabular:
     st.title('StarTable 🌌')
@@ -189,20 +193,37 @@ with tabular:
 with millenium:
     st.title('MilleniumSnap🌌')
     col1_fc, col2_fc = st.columns(2)
+    celestial_fc = False
     with col1_fc:
         st.subheader('Fill and upload your data ✍️')
 
         form_fc = st.form(key="hybrid_data")
         celestial_fc = form_fc.file_uploader("Upload your image",['jpeg'], accept_multiple_files=False)
-        alpha_fc = form_fc.text_input('Alpha',filling_data.alpha.values[0])
-        delta_fc = form_fc.text_input('Delta',filling_data.delta.values[0])
-        classif_fc = form_fc.text_input('Class',filling_data.label.values[0])
-        uv_fc = form_fc.text_input('Ultraviolet Filter',filling_data.u.values[0])
-        gf_fc = form_fc.text_input('Green Filter',filling_data.g.values[0])
-        rf_fc = form_fc.text_input('Red Filter',filling_data.r.values[0])
-        nf_fc = form_fc.text_input('Near Infrared Filter',filling_data.i.values[0])
-        irf_fc = form_fc.text_input('Infrared Filter',filling_data.z.values[0])
+        alpha_fc = form_fc.text_input('Alpha',filling_data_hybrid.alpha.values[0])
+        delta_fc = form_fc.text_input('Delta',filling_data_hybrid.delta.values[0])
+        classif_fc = form_fc.text_input('Class',filling_data_hybrid.label.values[0])
+        uv_fc = form_fc.text_input('Ultraviolet Filter',filling_data_hybrid.u.values[0])
+        gf_fc = form_fc.text_input('Green Filter',filling_data_hybrid.g.values[0])
+        rf_fc = form_fc.text_input('Red Filter',filling_data_hybrid.r.values[0])
+        nf_fc = form_fc.text_input('Near Infrared Filter',filling_data_hybrid.i.values[0])
+        irf_fc = form_fc.text_input('Infrared Filter',filling_data_hybrid.z.values[0])
         submit_st_fc = form_fc.form_submit_button("Run")
 
     with col2_fc:
         st.subheader("Predictions")
+        api_url_millenium = base_uri + 'redshift_from_image_and_params'
+        if submit_st_fc:
+            with st.spinner(text='Millenium Falcon model is working 🛸'):
+                img_bytes_millenium = celestial_fc.getvalue()
+                api_params_fc = {
+                    'alpha':alpha_fc,
+                    'delta':delta_fc,
+                    'g':uv_fc,
+                    'u':gf_fc,
+                    'r':rf_fc,
+                    'i':nf_fc,
+                    'z':irf_fc,
+                }
+                res_fc = rq.post(api_url_millenium, files={'file': img_bytes_millenium}, data=api_params_fc).json()
+
+                st.write(display_redshift(str(res_fc['prediction'])))
